6416d0dca4e2cdbb71df292b78bfe306cef734a4
boubacar2411 <81101759+boubacar2411@users.noreply.github.com>
2025-03-29 15:58:29 +0000

    fix: Correction de la structure du projet et configuration des tests

--- src/test/java/sn/app/ebank/EbankApplicationTests.java
+++ src/test/java/sn/app/ebank/EbankApplicationTests.java
@@ -3,7 +3,7 @@ package sn.app.ebank;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = EbankApplication.class)
 class EbankApplicationTests {
 
     @Test
