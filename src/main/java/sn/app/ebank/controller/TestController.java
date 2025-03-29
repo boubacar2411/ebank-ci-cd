3d82f6df3296f1215f641a752f828c45eee715dd
boubacar2411 <81101759+boubacar2411@users.noreply.github.com>
2025-03-29 16:45:51 +0000

    ci: Mise à jour du workflow Maven

---
+++ src/main/java/sn/app/ebank/controller/TestController.java
@@ -0,0 +1,13 @@
+package sn.app.ebank.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/test")
+    public String test() {
+        return "Application E-Bank is running!";
+    }
+} 
--- src/test/java/sn/app/ebank/EbankApplicationTests.java
+++ src/test/java/sn/app/ebank/EbankApplicationTests.java
@@ -2,8 +2,10 @@ package sn.app.ebank;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest(classes = EbankApplication.class)
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.yml")
 class EbankApplicationTests {
 
     @Test
